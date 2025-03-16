dbc02ec093c531d9194fe7bd0b5bc50c0d2dc527
Aleksander Moiseenko <alex.mois.developer@gmail.com>
2025-03-16 12:35:34 +0300

    feat: configure container

--- warehouse_management/container.py
+++ warehouse_management/container.py
@@ -1,19 +1,27 @@
+"""Модуль контейнера зависимостей для управления зависимостями приложения.
+
+Этот модуль использует `dependency_injector`, чтобы управлять созданием и передачей зависимостей
+в различных частях приложения.
+"""
+
 from dependency_injector import containers, providers
 
 from warehouse_management.infrastructure.database import SessionFactory
 from warehouse_management.infrastructure.repositories import SqlAlchemyOrderRepository, SqlAlchemyProductRepository
 from warehouse_management.infrastructure.unit_of_work import SqlAlchemyUnitOfWork
-from warehouse_management.settings import settings
 
 
 class AppContainer(containers.DeclarativeContainer):
-    """Контейнер инъекции зависимостей"""
+    """Контейнер инъекции зависимостей для управления компонентами приложения.
 
-    config = providers.Singleton(lambda: settings)
+    Этот контейнер управляет созданием сессий БД, репозиториев и Unit of Work.
+    """
+
+    config = providers.Configuration()
 
     session = providers.Singleton(SessionFactory)
 
     product_repository = providers.Singleton(SqlAlchemyProductRepository, session=session)
     order_repository = providers.Singleton(SqlAlchemyOrderRepository, session=session)
 
-    unit_of_work = providers.Singleton(SqlAlchemyUnitOfWork, session=session)
+    unit_of_work: providers.Singleton[SqlAlchemyUnitOfWork] = providers.Singleton(SqlAlchemyUnitOfWork, session=session)
