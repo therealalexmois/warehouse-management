270ddaa6a70f6915a0dfa4066d6cbf1fe57defa9
Aleksander Moiseenko <alex.mois.developer@gmail.com>
2025-03-16 13:21:32 +0300

    feat: refactor main

--- warehouse_management/main.py
+++ warehouse_management/main.py
@@ -1,17 +1,29 @@
+"""Основная точка входа в приложение для управления складом.
+
+Инициализирует базу данных и устанавливает инъекцию зависимостей для сервисного слоя.
+"""
+
+from dependency_injector.wiring import inject
+
 from warehouse_management.container import AppContainer
 from warehouse_management.domain.services import WarehouseService
 from warehouse_management.infrastructure.database import init_db
 
 
-def main() -> None:
-    """Главный модуль запуска и настройки приложения"""
-    init_db()
+@inject
+def main(container: 'AppContainer') -> None:
+    """Главный модуль запуска и настройки приложения.
 
+    Args:
+        container: Контейнер зависимостей приложения.
+    """
     product_repo = container.product_repository()
     order_repo = container.order_repository()
     uow = container.unit_of_work()
 
     warehouse_service = WarehouseService(product_repo, order_repo)
+
+    init_db()
 
     with uow:
         new_product = warehouse_service.create_product(name='Test Product', quantity=5, price=150.0)
@@ -20,7 +32,11 @@ def main() -> None:
 
 
 if __name__ == '__main__':
-    # TODO: Передать конфиг в container
-    # settings = container.settings()
+    from warehouse_management.settings import Settings
+
+    settings = Settings()
     container = AppContainer()
-    main()
+    container.config.from_pydantic(settings)
+    container.wire(modules=[__name__])
+
+    main(container)
