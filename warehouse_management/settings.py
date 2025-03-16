2b34cd81e764334e49a134ff80a67ee715870421
Aleksander Moiseenko <alex.mois.developer@gmail.com>
2025-03-16 11:21:43 +0300

    refactor: finish settings

--- warehouse_management/settings.py
+++ warehouse_management/settings.py
@@ -1,12 +1,48 @@
+"""Настройка параметров приложения с помощью Pydantic.
+
+Этот модуль определяет класс `Settings`, который загружает конфигурацию из переменных окружения
+переменных и дополнительного файла `.env.local` для локальной разработки.ariables and an optional `.env.local` file for local development.
+"""
+
+import os
+from enum import Enum
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-_ENV_PATH =  '.env.local'
-_ENV_PREFIX = 'warehouse_management_'
+DEFAULT_ENV = 'local'
+
+_ENV_PREFIX: str = 'WAREHOUSE_MANAGEMENT_'
+_ENV_FILE: str | None = '.env.local' if os.environ.get(f'{_ENV_PREFIX}ENV', DEFAULT_ENV) == DEFAULT_ENV else None
+
+
+class Env(str, Enum):
+    """Перечисление допустимых значений среды."""
+
+    LOCAL = 'local'
+    DEVELOPMENT = 'development'
+    PRODUCTION = 'production'
+
+
+class DatabaseSettings(BaseSettings):
+    """Настройки конфигурации базы данных."""
+
+    url: str = 'sqlite:///warehouse.db'
+
+    model_config = SettingsConfigDict(
+        env_prefix=_ENV_PREFIX,
+        env_file=_ENV_FILE,
+        extra='ignore',
+    )
 
 
 class Settings(BaseSettings):
-    DATABASE_URL = 'sqlite:///warehouse.db'
+    """Основные настройки приложения, содержащие все конфигурации."""
 
-    model_config = SettingsConfigDict(env_prefix=_ENV_PREFIX, env_file=_ENV_PATH, extra='ignore')
+    env: Env = Env.LOCAL
+    database: DatabaseSettings = DatabaseSettings()
 
-settings = Settings()
+    model_config = SettingsConfigDict(
+        env_prefix=_ENV_PREFIX,
+        env_file=_ENV_FILE,
+        extra='ignore',
+    )
